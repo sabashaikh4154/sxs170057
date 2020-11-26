39fab94e0ca871fce09230b6c602575645fccd92
sabashaikh4154 <61804572+sabashaikh4154@users.noreply.github.com>
2020-11-25 19:51:46 -0600

    Create program6.h

---
+++ program6.h
@@ -0,0 +1,19 @@
+
+/*
+ * Filename     program6.h
+ * Date         11/25/2020
+ * Author       Saba Shaikh
+ * Email        sxs170057@utdallas.edu
+ * Course       CS 3377.001
+ * Version      1.0
+ * Copyright    2020, All Rights Reserved
+ *
+ * Description
+ *
+ *   Demonstrate the ability to create a multi-file project that
+ *   is hosted in a GIT repository. Demonstrate the ability to
+ *   perform Binary File I/O. Demonstrate the ability to visualize
+ *   binary data using a CDK matrix
+ *
+ */
+
